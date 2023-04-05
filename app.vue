6526caeadb3c456ee5c9a223ba249f212c0c995c
MarcoHilliges <107026106+MarcoHilliges@users.noreply.github.com>
2023-04-05 22:08:30 +0200

    add main nav

--- app.vue
+++ app.vue
@@ -1,9 +1,15 @@
 <template>
-    <NavigationBarSimple />
     <NuxtLoadingIndicator />
-    <NuxtPage />
+    <div class="bg-gray-800 h-screen p-6">
+      <NavigationBarSimple />
+      <NuxtPage />
+    </div>
 </template>
 
 <script setup lang="ts">
 const route = useRoute()
 </script>
+
+<style lang="scss" scoped>
+
+</style>
