5b0755684068e6f9286db1d09a08544abe6e2f81
MarcoHilliges <107026106+MarcoHilliges@users.noreply.github.com>
2023-04-06 21:10:12 +0200

    sideNav

--- app.vue
+++ app.vue
@@ -1,8 +1,24 @@
 <template>
     <NuxtLoadingIndicator />
-    <div class="bg-gray-800 h-screen p-6">
-      <NavigationBarSimple />
-      <NuxtPage />
+    <div class="flex bg-gray-800 h-screen">
+      <div class="w-48">
+        <NuxtLink to="/" class="h-24 flex items-center justify-center">
+          <img class="h-16" src="@/assets/Logo-dark.png" alt="Logo">
+        </NuxtLink>
+        <NavigationBarSimple />
+      </div>
+      <div>
+        <header class="h-[50px]">
+          
+        </header>
+        <main class="w-[calc(100vw-200px)] h-[calc(100vh-100px)] overflow-auto bg-gray-700">
+          <NuxtPage />
+        </main>
+        <footer>
+
+        </footer>
+      </div>
+      
     </div>
 </template>
 
